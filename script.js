da6774599ebb43d41c74d4d1ffc42f787425440e
Austin McEachern <71673263+Austinmce20@users.noreply.github.com>
2021-01-04 18:40:47 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function ExampleJS(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var years = document.getElementById("num").value;
+    var skill = document.getElementById("skillLevel").value;
+    
+    alert(name + "\n" + email + "\n" + years + "\n" + skill);
+}
+
